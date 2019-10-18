9fe7db0c34100e2f9a614fdd51da4a5e5b795b76
SotaSuzuki <3.3sota.suzuki@gmail.com>
2019-10-19 07:52:03 +0900

    feat: add router and views

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,23 +2,53 @@ import React from 'react';
 import logo from '../../assets/images/logo.svg';
 import './App.css';
 
+import {
+  Home,
+  About,
+  Dashboard,
+} from 'views';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/views/About/About.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const About: FC = () => {
+  return (
+    <div className="About">
+      <h1>About</h1>
+    </div>
+  )
+}
+
+export default About
