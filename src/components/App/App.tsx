4586f8de0dcedcee0aebf268f9b03b8b24434431
SotaSuzuki <3.3sota.suzuki@gmail.com>
2019-10-26 22:14:41 +0900

    add css modules feature

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import logo from '../../assets/images/logo.svg'
-import styles from './App.module.css'
+import styles from './App.module.scss'
 
 import { Home, About, Dashboard } from 'views'
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 const App: React.FC = () => {
+  const routes = [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'About',
+      path: '/about',
+    },
+    {
+      name: 'Dashboard',
+      path: '/dashboard',
+    },
+  ]
+
   return (
     <div className={styles.module}>
       <Router>
@@ -14,17 +29,13 @@ const App: React.FC = () => {
           <img src={logo} className={styles.logo} alt="logo" />
         </header>
 
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
+        <nav className={styles.nav}>
+          <ul className={styles.nav__list}>
+            {routes.map(({ name, path }) => (
+              <li className={styles['nav__list-item']} key={name}>
+                <Link to={path}>{name}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
 
