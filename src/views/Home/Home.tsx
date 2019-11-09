4822a770d91555493e4126a905fedb3d4604d972
SotaSuzuki <3.3sota.suzuki@gmail.com>
2019-11-09 18:36:17 +0900

    try react-hooks-async

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,35 +1,51 @@
-import React, { FC, Suspense, useEffect, useState } from 'react'
+import React, { FC } from 'react'
 import styles from './Home.module.css'
 
-type Data = {
-  hello: string
+import { useFetch } from 'react-hooks-async'
+
+type UserInfoProps = {
+  id: string
 }
 
-const Home: FC = () => {
-  const [data, setData] = useState<any>(null)
-
-  useEffect(() => {
-    fetch('http://www.mocky.io/v2/5dc664e33800002f00cdeb61').then(response =>
-      setData(response),
+const UserInfo: FC<UserInfoProps> = ({ id }) => {
+  const url = `https://qiita.com/api/v2/users/${id}`
+  const { pending, error, result, abort } = useFetch(url)
+  if (pending) {
+    return (
+      <div>
+        Loading...<button onClick={abort}>Abort</button>
+      </div>
     )
-  }, [data])
+  }
+  if (error) {
+    return (
+      <div>
+        Error: {error.name} {error.message}
+      </div>
+    )
+  }
+
+  console.log(result)
 
   return (
-    <div className={styles.module}>
-      <h1 className={styles.title}>Home</h1>
-      <Suspense fallback={<div>loading...</div>}>
-        <Comp data={data} />
-      </Suspense>
+    <div>
+      <div>Name: {result.name}</div>
+      <img
+        src={result.profile_image_url}
+        alt={result.description}
+        width="100"
+      />
     </div>
   )
 }
 
-type CompProps = {
-  data: any
-}
-
-const Comp: FC<CompProps> = props => {
-  return <>{props.data && <span>response: {props.data.hello}</span>}</>
+const Home: FC = () => {
+  return (
+    <div className={styles.module}>
+      <h1 className={styles.title}>Home</h1>
+      <UserInfo id={'SotaSuzuki'} />
+    </div>
+  )
 }
 
 export default Home
